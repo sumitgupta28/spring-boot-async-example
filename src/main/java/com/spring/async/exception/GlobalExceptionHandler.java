486cc156e6a856f4489fba36f4ef0a0d985f1f51
Sumit Gupta <sumitgupta28@gmail.com>
2025-07-29 14:08:23 -0500

    Init

--- src/main/java/com/spring/async/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/async/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.spring.async.exception;
 
 import com.spring.async.model.OrderResponse;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
